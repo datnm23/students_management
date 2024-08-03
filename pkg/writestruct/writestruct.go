28397312d55a8a48f3bdbc6064e2fc49c23b3546
datnm23 <147710347+datnm23@users.noreply.github.com>
2024-08-03 12:17:28 +0700

    Update writestruct.go

--- pkg/writestruct/writestruct.go
+++ pkg/writestruct/writestruct.go
@@ -8,10 +8,5 @@ import (
 
 func WriteStruct(filepath string, content any) error {
 	data := fmt.Sprint(content)
-	err := os.WriteFile(filepath, []byte(data), 0644)
-
-	if err != nil {
-		log.Fatal(err)
-	}
-	return nil
+	return os.WriteFile(filepath, []byte(data), 0644)
 }
