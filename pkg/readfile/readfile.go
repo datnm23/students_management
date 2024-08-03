5e24e190935097659b06a31485344ca4620a1fb3
datnm23 <147710347+datnm23@users.noreply.github.com>
2024-08-03 12:16:48 +0700

    Update readfile.go

--- pkg/readfile/readfile.go
+++ pkg/readfile/readfile.go
@@ -8,8 +8,8 @@ import (
 func ReadFile(filepath string) (string, error) {
 	file, err := os.ReadFile(filepath)
 	if err != nil {
-		log.Fatal(err)
+		return "", err
 	}
 	fileString := string(file)
-	return fileString, err
+	return fileString, nil
 }
